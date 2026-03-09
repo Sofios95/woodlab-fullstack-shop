049adc3a3e8b18937148b0c2af4a0b556775b111
Kostas Sofios <sofios.konstantinos95@gmail.com>
2026-03-09 15:40:29 +0200

    Professional Structure: Split index.js into controllers and routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,13 +4,13 @@ import {
   addProduct, 
   updateProduct, 
   deleteProduct 
-} from "../controllers/productController.js";
+} from "../controllers/ProductController.js";
 
 const router = express.Router();
 
-router.get("/home", getAllProducts);  
-router.post("/admin", addProduct);      
-router.put("/admin/:id", updateProduct);   
-router.delete("/admin/:id", deleteProduct); 
+router.get("/home", getAllProducts);
+router.post("/admin", addProduct);
+router.put("/admin/:id", updateProduct);
+router.delete("/admin/:id", deleteProduct);
 
 export default router;
