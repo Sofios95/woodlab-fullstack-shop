fb5a8c215b015fd5321f34a32c10f8b5f9f7abdb
Kostas Sofios <sofios.konstantinos95@gmail.com>
2026-03-09 15:33:02 +0200

    Professional Structure: Split index.js into controllers and routes

---
+++ backend/config/db.js
@@ -0,0 +1,11 @@
+import pkg from "pg";
+const { Pool } = pkg;
+
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+export default db;
